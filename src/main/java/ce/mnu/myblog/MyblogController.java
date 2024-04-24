bf47f30545c7f998eaea5c08a57ff66100ecba3b
lifeOneshot <129479333+lifeOneshot@users.noreply.github.com>
2024-04-24 14:23:59 +0900

    test
    .

--- src/main/java/ce/mnu/myblog/MyblogController.java
+++ src/main/java/ce/mnu/myblog/MyblogController.java
@@ -13,6 +13,11 @@ public class MyblogController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@GetMapping(value = {"", "/"})
+	public String main(Model model) {
+		return "main";
+	}
+	
 	@GetMapping(path="/signup")
 	public String signup(Model model) {
 		model.addAttribute("user", new BlogUser());
@@ -26,19 +31,25 @@ public class MyblogController {
 		return "signup_done";
 	}
 	
-	@PostMapping(path="/find")
-	public String findUser(@RequestParam(name="email") String email, HttpSession session, Model model, RedirectAttributes rd){
-		BlogUser user = userRepository.findByEmail(email);
-		if(user != null) {
-			model.addAttribute("user", user);
-			return "find_done";
-		}
-		rd.addFlashAttribute("reason", "wrong email");
-		return "redirect:/error";
-	}
-	
-	@GetMapping(path="/find")
-	public String find() {
-		return "find_user";
-	}
+//	@PostMapping(path="/login")
+//	public String login(@RequestParam(name="email") String email, @RequestParam(name="passwd") String passwd, 
+//			HttpSession session, RedirectAttributes rd) {
+//		model.addAttribute("user", new BlogUser());		
+//		return "login";
+//	}
+//	@PostMapping(path="/find")
+//	public String findUser(@RequestParam(name="email") String email, HttpSession session, Model model, RedirectAttributes rd){
+//		BlogUser user = userRepository.findByEmail(email);
+//		if(user != null) {
+//			model.addAttribute("user", user);
+//			return "find_done";
+//		}
+//		rd.addFlashAttribute("reason", "wrong email");
+//		return "redirect:/error";
+//	}
+//	
+//	@GetMapping(path="/find")
+//	public String find() {
+//		return "find_user";
+//	}
 }
