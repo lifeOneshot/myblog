a754ecba3b1688ef0cc819b56cb256e233d9636c
lifeOneshot <129479333+lifeOneshot@users.noreply.github.com>
2024-04-29 16:41:01 +0900

    aritcle 베이스
    기능없음

---
+++ src/main/java/ce/mnu/myblog/Article.java
@@ -0,0 +1,31 @@
+package ce.mnu.myblog;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private Long num;
+
+    @Column(length=20, nullable=false)
+    private String author;
+
+    @Column(length=50, nullable=false)
+    private String title;
+
+    @Column(length=2048, nullable=false)
+    private String body;
+
+    public Long getNum() { return num; }
+	public void setNum(Long n) { num=n; }
+	
+	public String getAuthor() { return author; }
+	public void setAuthor(String a) { author=a; }
+	
+	public String getTitle() { return title; }
+	public void setTitle(String t) { title=t; }
+	
+	public String getBody() { return body; }
+	public void setBody(String b) { body=b; }
+}
---
+++ src/main/java/ce/mnu/myblog/ArticleRepository.java
@@ -0,0 +1,12 @@
+package ce.mnu.myblog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
+
